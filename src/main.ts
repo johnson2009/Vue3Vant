34c1c13590ca0e1ae1fcb629e1bc62b9989adaea
Johnson <Johnson.Sun@mkcorp.com>
2021-12-14 11:31:03 +0800

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {setupVantComponent} from "./plugins/vant";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+app.use(router);
+
+setupVantComponent(app);
+
+app.mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div>
+    <WorkList />
   </div>
+  </div>
+  
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import WorkList from "@/components/WorkList.vue";
+import Hello from "@/components/Hello.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
     HelloWorld,
+    WorkList
   },
 });
 </script>
